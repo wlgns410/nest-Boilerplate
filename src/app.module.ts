f9e0b2dce27858cf71fb071060d670b4595ae58b
wlgns410 <wlgns410@gmail.com>
2022-09-12 12:09:19 +0900

    nestjs mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CatsModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGODATABASE, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
